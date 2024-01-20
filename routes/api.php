ce71b7e31760812e89a8d718aa88765cc90e2043
Bayram Cavlak <bcavlak@gmail.com>
2024-01-20 12:37:15 +0300

    Add RandomUserMiddleware to the API routes for product management

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('products')->group(function () {
+    Route::post('/store', [ProductController::class, "store"])->name('products.store')->middleware('random.user');
+    Route::put('/{id}/update', [ProductController::class, "update"])->name('products.update');
+    Route::delete('/{id}/destroy', [ProductController::class, "destroy"])->name('products.destroy');
+    Route::get('/', [ProductController::class, "index"])->name('products.index');
+    Route::get('/{id}/show', [ProductController::class, "show"])->name('products.show');
 });
